cfd51a665b8a267e45a50f36782be882e522e85d
WannXJ <hackerbaiktlt@gmail.com>
2024-10-11 20:32:53 +0700

    Add files via upload

---
+++ tugas_dasprog.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    string nama;
+    string nim;
+    string kelas;
+    string prodi;
+    string alamat;
+    string ttl;
+    
+    cout<<endl;
+
+    cout<<"-----------------------"<<endl;
+    cout<<"|   BIODATA MAHASISWA  |"<<endl;
+    cout<<"-----------------------"<<endl;
+    cout<<"|Nama   :Muh Apriawan"<<nama<<endl;
+    cout<<"|NIM    :F55124027"<<nim<<endl;
+    cout<<"|Kelas  :TI A"<<kelas<<endl;
+    cout<<"|Prodi  :S1 Teknik Informatika"<<prodi<<endl;
+    cout<<"|Alamat :Jalan L Gadi"<<alamat<<endl;
+    cout<<"|TTL    :Toli-toli, Sulawesi Tengah"<<ttl<<endl;
+}
